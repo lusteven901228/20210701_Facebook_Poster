74eec8f4e5bcf4eac7d9af3802a43ec3bf778926
lusteven901228 <lusteven901228@gmail.com>
2022-09-01 15:43:14 +0800

    2022 update
    1. Delete multiprocessing since the limit is the upload speed
    2. Added fail detection in case of upload limit
    3. Added input() at last for result viewing

--- io.py
+++ io.py
@@ -1,19 +1,14 @@
-import json
-from multiprocessing import Process, Queue
 from upload_files import upload
-q = Queue()
-def worker():
-    while not q.empty():
-        upload(q.get())
-if __name__ == '__main__':  
+if __name__ == '__main__':
     print('execute value: range(start, end)')
     x = int(input('start(yy-%%%):'))
     y = int(input('end(yy-%%%):'))
     for i in range(x,y):
-        q.put(i)
-    p = []
-    for i in range(16):
-        p.append(Process(target=worker()))
-        p[i].start()
-    for i in range(16):
-        p[i].join()
+        t = upload(i)
+        if t:
+            print(f'{i} upload failed')
+            print(f'Finished posting {i-x} posts in range({x},{i})')
+            break
+    else:
+        print(f"Finished posting {y-x} posts in range({x},{y})")
+    input()
